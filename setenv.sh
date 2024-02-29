e5c7ccb2c4d7c0da6edc48f8dcd260c73a8fc34e
engrharisijaz <92294452+engrharisijaz@users.noreply.github.com>
2024-02-29 17:43:17 +0500

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY=fdkjshfhjsdfdskfdsfdcbsjdkfdsdf
 export DEBUG=True
-export APP_SETTINGS=config.PrductionConfig
+export APP_SETTINGS=config.ProductionConfig
 export DATABASE_URL=sqlite:///db.sqlite
 export FLASK_APP=src
 export FLASK_DEBUG=1
