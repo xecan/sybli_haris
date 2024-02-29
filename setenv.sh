bd8daeef141ce8efeb4f83520070c3add8e0d4f1
engrharisijaz <92294452+engrharisijaz@users.noreply.github.com>
2024-02-29 17:29:17 +0500

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -4,7 +4,4 @@ export APP_SETTINGS=config.PrductionConfig
 export DATABASE_URL=sqlite:///db.sqlite
 export FLASK_APP=src
 export FLASK_DEBUG=1
-<<<<<<< HEAD
-=======
 
->>>>>>> ddc86f693d5a5b07634fb4d5048994a5ee7586cc
